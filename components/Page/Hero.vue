a10ea645b5edee641e5cf1932e6d4a209a8d228f
Anders <afck97@hotmail.com>
2024-12-09 18:29:31 +0100

    fixed frontprage hero

--- components/Page/Hero.vue
+++ components/Page/Hero.vue
@@ -1,54 +1,27 @@
 <template>
-  <div class="hero__content container">
-    <h2 class="hero__content__title">
-      Stay Updated with the latest Tech News!
-    </h2>
-  </div>
+  <h2 class="hero">
+    Stay Updated with the latest Tech News!
+  </h2>
 </template>
 
 <style lang="scss" scoped>
 .hero {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 500px;
-  background-size: cover;
+  font-size: 3rem;
+  text-align: center;
+  padding: $space-2;
+  margin-top: 60px;
 
-    @media screen and (max-width: $breakpoint-md) {
-      height: 400px;
-    }
-
-    &__content {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-      gap: $space-4;
-
-      @media screen and (max-width: $breakpoint-sm) {
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-      }
-
-      &__title {
-
-      font-size: 3.5rem;
-      margin-top: 120px;
-
-        @media screen and (max-width: $breakpoint-md) {
+  @media screen and (max-width: $breakpoint-md) {
           font-size: 2.5rem;
           margin-top: 80px;
 
         }
 
-        @media screen and (max-width: $breakpoint-sm) {
+  @media screen and (max-width: $breakpoint-sm) {
           font-size: 2rem;
           margin-top: 60px;
 
         }
-      }
-    }
 
 }
 </style>
